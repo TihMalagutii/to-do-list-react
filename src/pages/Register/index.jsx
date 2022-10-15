f034642bdb85e86cfb6b3b62753455c707105366
TihMalagutii <euphoriabeats7146090532@gmail.com>
2022-10-15 02:00:54 -0300

    feat: :memo: add Register page

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,7 +1,60 @@
-import styles from './register.module.css'
+import { useState } from 'react'
+import styles from '../Home/home.module.css'
+import { Link } from 'react-router-dom'
 
 export default function Register() {
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+
+    if(name !== '' && email !== '' && password !== ''){
+      alert('funcionando')
+    } else {
+      alert('Incorrect Email/Password')
+    }
+    
+  }
+
   return (
-    <div>Register</div>
+    <div className={styles.container}>
+
+      <h1>TO-DO List</h1>
+
+      <form onSubmit={handleRegister}>
+
+        <input 
+          type="text"
+          placeholder='Name'
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+
+        <input 
+          type="text"
+          placeholder='Email'
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+
+        <input 
+          type="password"
+          placeholder='Password'
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+
+        <button type='submit'>REGISTER</button>
+
+      </form>
+
+      <Link className={styles.registerLink} to="/">
+        Have an account? <strong>Log In</strong>
+      </Link>
+
+    </div>
   )
 }
